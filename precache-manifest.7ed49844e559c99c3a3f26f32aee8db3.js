1fe085860a1b3c5d3709e11d17431e40fb96c0af
Britz Wang <britz.wang@kooppi.com>
2021-11-15 13:05:05 +0800

    update

---
+++ precache-manifest.7ed49844e559c99c3a3f26f32aee8db3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49f0c1ce96d7e20b45970ed162914ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ad9f785685fa7b8351a",
+    "url": "/static/css/2.63269516.chunk.css"
+  },
+  {
+    "revision": "cafb4335f7666b79c07d",
+    "url": "/static/css/main.0cde9a0f.chunk.css"
+  },
+  {
+    "revision": "3ad9f785685fa7b8351a",
+    "url": "/static/js/2.201c0e38.chunk.js"
+  },
+  {
+    "revision": "cafb4335f7666b79c07d",
+    "url": "/static/js/main.5cd9216f.chunk.js"
+  },
+  {
+    "revision": "bb3e1bb4977e9577515e",
+    "url": "/static/js/runtime-main.7cac21eb.js"
+  },
+  {
+    "revision": "15c01e256a564b7980e5af1f1032cd13",
+    "url": "/static/media/PlayfairDisplay-Regular.15c01e25.woff2"
+  },
+  {
+    "revision": "872cbd8fc3fab4ee027f209ce343e882",
+    "url": "/static/media/PlayfairDisplay-Regular.872cbd8f.woff"
+  },
+  {
+    "revision": "84ec15b82d7f8368326ffe547841aa4e",
+    "url": "/static/media/bg-SPY.84ec15b8.jpg"
+  },
+  {
+    "revision": "c876672dafed96eb8259637ddd1882b8",
+    "url": "/static/media/bg-demo.c876672d.jpg"
+  },
+  {
+    "revision": "a10ee4a7e90ba79129b49b9625a605a3",
+    "url": "/static/media/bg-pica.a10ee4a7.jpg"
+  },
+  {
+    "revision": "c149fbeba7ac1ef2845a15c5572156bf",
+    "url": "/static/media/bg-sonopro.c149fbeb.jpg"
+  },
+  {
+    "revision": "ebadf7a42e930218d40c5b2916dbbd01",
+    "url": "/static/media/bg-viscovery.ebadf7a4.jpg"
+  },
+  {
+    "revision": "1b678d6cabdc5a8c090c3695b09c34dd",
+    "url": "/static/media/bg-wt.1b678d6c.jpg"
+  },
+  {
+    "revision": "478d8cfa8e9d1b9806d128e4dfe3361d",
+    "url": "/static/media/britz.478d8cfa.jpg"
+  },
+  {
+    "revision": "c05b825b4c254c44290a7bbbcd8d11d5",
+    "url": "/static/media/jenna.c05b825b.jpg"
+  }
+]);
---
+++ swirl.js
@@ -0,0 +1,226 @@
+'use strict';
+
+const particleCount = 700;
+// const particleCount = window.innerWidth / 2;
+const particlePropCount = 9;
+const particlePropsLength = particleCount * particlePropCount;
+const rangeY = 150;
+const baseTTL = 50;
+const rangeTTL = 200;
+const baseSpeed = 0.5;
+// const rangeSpeed = window.innerWidth / 1.5 * 100
+const rangeSpeed = 0.1;
+const baseRadius = 1;
+const rangeRadius = 4;
+const baseHue = 150;
+const rangeHue = 90;
+const noiseSteps = 10;
+const xOff = 0.00125;
+const yOff = 0.00125;
+const zOff = 0.0005;
+// const backgroundColor = 'hsla(260,40%,5%,1)';
+const backgroundColor = 'rgba(11,19,46,1)'
+
+// let rangeY;
+let container;
+let canvas;
+let ctx;
+let center;
+let gradient;
+let tick;
+let simplex;
+let particleProps;
+let positions;
+let velocities;
+let lifeSpans;
+let speeds;
+let sizes;
+let hues;
+
+function setup() {
+	createCanvas();
+  resize();
+  initParticles();
+	draw();
+  window.addEventListener('resize', resize);
+}
+
+function initParticles() {
+  tick = 0;
+  simplex = new SimplexNoise();
+  particleProps = new Float32Array(particlePropsLength);
+
+  let i;
+  
+  for (i = 0; i < particlePropsLength; i += particlePropCount) {
+    initParticle(i);
+  }
+}
+
+function initParticle(i) {
+  let x, y, vx, vy, life, ttl, speed, radius, hue;
+
+  x = rand(canvas.a.width);
+  y = center[1] + randRange(rangeY);
+  vx = 0;
+  vy = 0;
+  life = 0;
+  ttl = baseTTL + rand(rangeTTL);
+  speed = baseSpeed + rand(rangeSpeed);
+  radius = baseRadius + rand(rangeRadius);
+  hue = baseHue + rand(rangeHue);
+
+  particleProps.set([x, y, vx, vy, life, ttl, speed, radius, hue], i);
+}
+
+function drawParticles() {
+  let i;
+
+  for (i = 0; i < particlePropsLength; i += particlePropCount) {
+    updateParticle(i);
+  }
+}
+
+function updateParticle(i) {
+  let i2=1+i, i3=2+i, i4=3+i, i5=4+i, i6=5+i, i7=6+i, i8=7+i, i9=8+i;
+  let n, x, y, vx, vy, life, ttl, speed, x2, y2, radius, hue;
+
+  x = particleProps[i];
+  y = particleProps[i2];
+  n = simplex.noise3D(x * xOff, y * yOff, tick * zOff) * noiseSteps * TAU;
+  vx = lerp(particleProps[i3], cos(n), 0.5);
+  vy = lerp(particleProps[i4], sin(n), 0.5);
+  life = particleProps[i5];
+  ttl = particleProps[i6];
+  speed = particleProps[i7];
+  x2 = x + vx * speed;
+  y2 = y + vy * speed;
+  radius = particleProps[i8];
+  hue = particleProps[i9];
+
+  drawParticle(x, y, x2, y2, life, ttl, radius, hue);
+
+  life++;
+
+  particleProps[i] = x2;
+  particleProps[i2] = y2;
+  particleProps[i3] = vx;
+  particleProps[i4] = vy;
+  particleProps[i5] = life;
+
+  (checkBounds(x, y) || life > ttl) && initParticle(i);
+}
+
+function drawParticle(x, y, x2, y2, life, ttl, radius, hue) {
+  ctx.a.save();
+  ctx.a.lineCap = 'round';
+  ctx.a.lineWidth = radius;
+  ctx.a.strokeStyle = `hsla(${hue},100%,60%,${fadeInOut(life, ttl)})`;
+  ctx.a.beginPath();
+  ctx.a.moveTo(x, y);
+  ctx.a.lineTo(x2, y2);
+  ctx.a.stroke()
+  ctx.a.closePath();
+  ctx.a.restore();
+}
+
+function checkBounds(x, y) {
+	return(
+		x > canvas.a.width ||
+		x < 0 ||
+		y > canvas.a.height ||
+		y < 0
+	);
+}
+
+function createCanvas() {
+  container = document.querySelector('.bg-canvas');
+	canvas = {
+		a: document.createElement('canvas'),
+		b: document.createElement('canvas')
+	};
+	canvas.b.style = `
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	`;
+	container.appendChild(canvas.b);
+	ctx = {
+		a: canvas.a.getContext('2d'),
+		b: canvas.b.getContext('2d')
+  };
+  center = [];
+}
+
+function resize() {
+	const { innerWidth, innerHeight } = window;
+	
+	canvas.a.width = innerWidth;
+  canvas.a.height = innerHeight;
+
+  ctx.a.drawImage(canvas.b, 0, 0);
+
+	canvas.b.width = innerWidth;
+  canvas.b.height = innerHeight;
+  
+  ctx.b.drawImage(canvas.a, 0, 0);
+
+  center[0] = 0.5 * canvas.a.width;
+  center[1] = 0.5 * canvas.a.height;
+
+  // rangeY = Math.max(100, canvas.a.height * 0.5 / 2)
+}
+
+function renderGlow() {
+  ctx.b.save();
+  ctx.b.filter = 'blur(8px) brightness(150%)';
+  ctx.b.globalCompositeOperation = 'lighter';
+  ctx.b.drawImage(canvas.a, 0, 0);
+  ctx.b.restore();
+
+  ctx.b.save();
+  ctx.b.filter = 'blur(4px) brightness(150%)';
+  ctx.b.globalCompositeOperation = 'lighter';
+  ctx.b.drawImage(canvas.a, 0, 0);
+  ctx.b.restore();
+}
+
+function renderToScreen() {
+  ctx.b.save();
+  ctx.b.globalCompositeOperation = 'lighter';
+  ctx.b.drawImage(canvas.a, 0, 0);
+  ctx.b.restore();
+}
+
+function draw() {
+  tick++;
+
+  ctx.a.clearRect(0, 0, canvas.a.width, canvas.a.height);
+
+  // ctx.b.fillStyle = backgroundColor;
+
+  // radial-gradient(ellipse at center, rgba(20,58,127,0) 30%,rgba(11,19,46,0.8) 60%,rgba(0, 0, 0,1) 100%)
+  
+  var grd = ctx.b.createRadialGradient(canvas.a.width / 2, canvas.a.height / 2, 50, canvas.a.width / 2, canvas.a.height / 2, Math.max(canvas.a.width, canvas.a.height) / 2);
+  grd.addColorStop(0, 'rgb(20,58,127)');
+  grd.addColorStop(0.6, 'rgb(11,19,46)');
+  grd.addColorStop(1, 'rgb(11,19,46)');
+  ctx.b.fillStyle = grd;
+  ctx.b.fillRect(0, 0, canvas.a.width, canvas.a.height);
+
+  drawParticles();
+  renderGlow();
+  renderToScreen();
+
+	window.requestAnimationFrame(draw);
+}
+
+function destroy() {
+  container.querySelector('canvas').remove();
+  window.removeEventListener('resize', resize);
+}
+
+window.addEventListener('bannerBgShow', setup);
+window.addEventListener('bannerBgDestroy', destroy);
